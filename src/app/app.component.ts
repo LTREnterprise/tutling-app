f4685119b4d6a02397f60760a2ef4ff61a2e6302
Your Name <ltrenterprise001@gmail.com>
2019-04-03 21:06:57 +0000

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
@@ -10,8 +10,17 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private androidPermissions: AndroidPermissions, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      androidPermissions.requestPermissions(
+        [
+          androidPermissions.PERMISSION.CAMERA, 
+          androidPermissions.PERMISSION.CALL_PHONE, 
+          androidPermissions.PERMISSION.GET_ACCOUNTS, 
+          androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE, 
+          androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE
+        ]
+      );
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
